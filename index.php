40ac8227415255d185a6f776727a39730b482916
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-11-18 21:29:46 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Phoenyx Studio Test Endpoints</title>
+  </head>
+  <body>
+    <div>
+      <h1>Endpoints for testing network applications</h1>
+        <ul>
+            <li><a href="/blank-page.php">Blank page</a> - use this test endpoint to get sure that your application does not consider blank page returned with code 200 as error</li>
+            <li><a href="/redirect-301-source.php">Redirect source</a> - if your application provides automatic redirect this location will lead you to <a href="/redirect-301-target">Redirect target</a>.</li>
+        </ul>
+    </div>
+  </body>
+</html>

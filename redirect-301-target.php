911b0fef41d0af6e7ed25fda705e490349f6e606
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-11-18 21:35:44 +0200

    Redirect 301 target

---
+++ redirect-301-target.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Redirect 301 Target - Phoenyx Studio Test Endpoints</title>
+  </head>
+  <body>
+    <div>
+      <h1>Target of 301 redirect</h1>
+      <p>If you got here it means that automatic redirect worked successfully</p>
+    </div>
+  </body>
+</html>

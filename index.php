b6490d1beb6e2fc7ea50246de968e0519246509d
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-11-18 21:40:25 +0200

    Fixed index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
       <h1>Endpoints for testing network applications</h1>
         <ul>
             <li><a href="/blank-page.php">Blank page</a> - use this test endpoint to get sure that your application does not consider blank page returned with code 200 as error</li>
-            <li><a href="/redirect-301-source.php">Redirect source</a> - if your application provides automatic redirect this location will lead you to <a href="/redirect-301-target">Redirect target</a>.</li>
+            <li><a href="/redirect-301-source.php">Redirect source</a> - if your application provides automatic redirect this location will lead you to <a href="/redirect-301-target.php">Redirect target</a>.</li>
         </ul>
     </div>
   </body>
